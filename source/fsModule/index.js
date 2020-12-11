06748c2397e9a5bb019c2db72a3cb444cc63c62d
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-12-11 15:49:06 +0100

    rename file

--- source/fsModule/index.js
+++ source/fsModule/index.js
@@ -4,24 +4,77 @@ var http=require('http');
 
 var server=http.createServer(function(req,res){
 
-    if(req.url="/"){
-        // asynochronize way calling
+    if(req.url=="/")
+    {
+// read file #############################
+
+        // asynochronize way calling ########## start
         // call back have to write for asychronization
         // fs.readFile("Home.html",function(error,data){
         //     res.writeHead(200,{'Content-Type':'text/html'});
         //     res.write(data);
         //     res.end();
         // });
+// asynochronize way calling ########## end 
 
-        // Synchronization has no call back method
-        let myData=fs.readFileSync('Home.html');
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(myData);
-        res.end();
+// Synchronization has no call back method ############# start
+        // let myData=fs.readFileSync('Home.html');
+        // res.writeHead(200,{'Content-Type':'text/html'});
+        // res.write(myData);
+        // res.end();
+ // Synchronization end ############### 
 
-    }
 
+// write file ###################### Ayschronization way start
+//    fs.writeFile('demo.txt','Hello World Node JS OK',function(error){
+//        if(error){
+//            res.writeHead(200,{'Content-Type':'text/html'});
+//            res.write('File Write Fail');
+//            res.end();
+//        }
+//        else{
+//            res.writeHead(200,{'Content-Type':'text/html'});
+//            res.write('File Write Success')
+//            res.end();
+//        }
+//    })
 
+   // ################################# Ayschronization way end
+
+   // Synchronization way file write start ###########
+//    let error=fs.writeFileSync('demoSync.txt','This is snychro');
+  
+//    if(error){
+//                res.writeHead(200,{'Content-Type':'text/html'});
+//                res.write('File Write Fail');
+//                res.end();
+//             }
+//             else{
+//                res.writeHead(200,{'Content-Type':'text/html'});
+//                res.write('File Write Success')
+//                res.end();
+//             }
+        
+   // Synchronization way file write end ###########
+
+   // rename file ######################## Ayschronization way start
+    fs.rename('demo.txt','demoNew.txt',function(error){
+
+        if(error){
+                 res.writeHead(200,{'Content-Type':'text/html'});
+                 res.write('File Rename Fail');
+                 res.end();
+        }
+        else{
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write('File Rename Success')
+                res.end();
+            }
+
+    })
+   //######################## Ayschronization way end
+
+}
 });
 
 server.listen(2020);
