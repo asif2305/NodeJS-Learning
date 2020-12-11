419ece23276df45850a673d2c21d0a20fcc7d8dc
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-12-11 14:41:30 +0100

    read file in synchronization way

--- source/fsModule/index.js
+++ source/fsModule/index.js
@@ -6,14 +6,23 @@ var server=http.createServer(function(req,res){
 
     if(req.url="/"){
         // asynochronize way calling
-        fs.readFile("Home.html",function(error,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            res.end();
-        });
+        // call back have to write for asychronization
+        // fs.readFile("Home.html",function(error,data){
+        //     res.writeHead(200,{'Content-Type':'text/html'});
+        //     res.write(data);
+        //     res.end();
+        // });
+
+        // Synchronization has no call back method
+        let myData=fs.readFileSync('Home.html');
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(myData);
+        res.end();
+
     }
+
 
 });
 
-server.listen(4040);
+server.listen(2020);
 console.log("server run success");
