4355932714593f95770ae6c55a546be2111111fc
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2020-12-11 14:34:01 +0100

    first page using node js

---
+++ source/fsModule/index.js
@@ -0,0 +1,19 @@
+// import file system module
+var fs=require('fs');
+var http=require('http');
+
+var server=http.createServer(function(req,res){
+
+    if(req.url="/"){
+        // asynochronize way calling
+        fs.readFile("Home.html",function(error,data){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        });
+    }
+
+});
+
+server.listen(4040);
+console.log("server run success");
